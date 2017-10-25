1080bb937334252253bb56bc475afbd1852b5781
fadillasa <32873305+fadillasa@users.noreply.github.com>
2017-10-25 10:40:31 +0700

    Update soal4.c

--- soal4.c
+++ soal4.c
@@ -8,34 +8,36 @@
 *compile dengan cara gcc -pthread -o [output] input.c *
 *******************************************************/
 
-pthread_t tid[3];//inisialisasi array untuk menampung thread dalam kasus ini ada 3 thread
+pthread_t tid[100];
 
 
 void* faktorial(void *arg)
 {
     int i, iter=1;
-    int *num=(int*) arg;
+    int *num=(void *)arg;
     pthread_t id=pthread_self();
   
-   // if(pthread_equal(id,tid[0]))//thread untuk menjalankan counter
-   // {
-        for(i=*num;i>0;i--)
+        for(i=*num;i>1;i--)
         {
             iter*=i;
         }
 	printf("Hasil %d! = %d\n",*num,iter);
-   // }
-   // else
-   // {
-        
-   // }
+  
     return NULL;
 }
 int main(void)
 {
-    int i=0,N[]={2,3,4};
+    int i=0,j=0,angka,N[100];
+    char tes;
     int err;
-    while(i<3)//looping membuat thread 3x
+    while(1){
+    	scanf("%d",&angka);
+	scanf("%c",&tes);
+	N[j]=angka;
+	j++;
+	if(tes=='\n') break;
+    }
+    while(i<j)
     {
         err=pthread_create(&(tid[i]),NULL,&faktorial,&N[i]);//membuat thread
         if(err!=0)//cek error
@@ -49,10 +51,10 @@ int main(void)
         i++;
     }
     i=0;
-    while(i<3)
-	{
-	    pthread_join(tid[i],NULL);
-	    i++;
-	}
+    while(i<j)
+    {
+	pthread_join(tid[i],NULL);
+	i++;
+    }
     return 0;
 }
